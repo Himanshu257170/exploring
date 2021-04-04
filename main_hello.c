899ef8a1627fde55102417f0deaa0055b001de20
Himanshu <himanshu.kaushish14@gmail.com>
2021-04-04 23:14:37 +0530

    New code added

---
+++ main_hello.c
@@ -0,0 +1,16 @@
+/**
+ * @file main_hello.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+int main()
+{
+    printf("Welcome to Github");
+    return 0;
+}
